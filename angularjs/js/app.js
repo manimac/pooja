82b7bcbf2e581ead556fbc5bdba092cbbafda64d
manimac <manimaccse@gmail.com>
2019-04-06 15:27:12 +0530

    angular routing

---
+++ angularjs/js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('erp',['ui.router']);
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/error");
+    $stateProvider
+    .state('home2',
+        {
+            url : '/home3',
+            templateUrl : 'views/home.html'
+        }
+    )
+    .state('about',
+        {
+            url : '/about',
+            templateUrl : 'views/about.html'
+        }
+    )
+    .state('error',
+        {
+            url : '/error',
+            templateUrl : 'views/error.html'
+        }
+    )
+})
